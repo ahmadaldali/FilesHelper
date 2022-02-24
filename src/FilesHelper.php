ac9c71a8ecbceda6be02abda0026b8c80c8dbbdf
ahmadaldali <ahmadaldali88@gmail.com>
2022-02-24 16:05:48 +0300

    Handling if no file is uploaded

--- src/FilesHelper.php
+++ src/FilesHelper.php
@@ -20,6 +20,8 @@ class FilesHelper
     public static function fileUpload($file, $folder_name)
     {
         try {
+            //no uploaded file
+            if (!$file) throw new Exception('No File Found');
             //generate a random name
             $file_name = Str::random(10);
             //get the name/ name of uploaded image
@@ -30,16 +32,16 @@ class FilesHelper
             $full_file_name = $file_name . '.' . $ext;
             //path the folder + generated name + extension
             $full_path = $folder_name . '/' . $full_file_name;
-            //save in app/storage/public/folder_name folder
-            $file = Storage::disk('public')->put($full_path, File::get($file));
-            if(!$file) throw New Exception('Error Saving');
+            //save in storage/app/public/folder_name folder
+            $file = Storage::put($full_path, File::get($file));
+            if (!$file) throw new Exception('Error Saving');
             //get the size file
-            $size = Storage::disk('public')->size($full_path);
+            $size = Storage::size($full_path);
             //return result
             $data = [
                 'file' => $file,
                 'origin_name' => $origin_name,
-                'full_path' => 'storage/app/public/' . $full_path,
+                'full_path' => '/storage/app/public/' . $full_path,
                 'extension' => $ext,
                 'size' => $size
             ];
@@ -48,7 +50,6 @@ class FilesHelper
             Log::info('error upload file/fileUploadTrait: ' . $e->getMessage());
             return null;
         } //catch
-
-    } //method
+    } //fileUpload
 
 }//class
